ffdd9dcb9d8826aba76b9fc6926b6ad4deb479d1
maxinakenty <mgreen097@gmail.com>
2018-01-23 20:30:08 +0300

    fixed

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { render } from 'react-dom';
+import { Provider, connect } from 'react-redux';
 import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import App from './containers/App';
-import rootReducer from './reducers';
+
+const rootReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
 /* eslint-disable no-underscore-dangle */
 const store = createStore(
@@ -12,7 +22,35 @@ const store = createStore(
 );
 /* eslint-enable */
 
-console.log('store-----------', store.getState());
+const Counter = ({ state, onIncrement, onDecrement }) => (
+  <div className="coutner">
+    <h1>Simple counter</h1>
+    <h3>{state}</h3>
+    <button onClick={() => onIncrement} />
+    <button onClick={() => onDecrement} />
+  </div>
+);
+
+Counter.propTypes = {
+  onIncrement: PropTypes.func.isRequired,
+  onDecrement: PropTypes.func.isRequired,
+  state: PropTypes.number.isRequired,
+};
+
+const mapStateToProp = ({ state }) => ({
+  state,
+});
+
+const mapDispatchToProps = ({ dispatch }) => ({
+  onIncrement: () => {
+    dispatch({ type: 'INCREMENT' });
+  },
+  onDecrement: () => {
+    dispatch({ type: 'DECREMENT' });
+  },
+});
+
+const App = connect(mapStateToProp, mapDispatchToProps)(Counter);
 
 render(
   <Provider store={store}>
