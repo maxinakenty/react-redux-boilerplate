096b58f93dcb9ea2f9e87675caa852a2a3912aa9
maxinakenty <mgreen097@gmail.com>
2018-01-23 18:37:33 +0300

    fixed

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,17 +5,17 @@ import { hot } from 'react-hot-loader';
 import PropTypes from 'prop-types';
 import { increment, decrement } from '../actions';
 
-const Counter = ({ state, onIncrement, onDecrement }) => (
-  <div className="App">
-    <h1>Simple counter</h1>
-    <h2>{state}</h2>
-    <button onClick={onIncrement}>+</button>
-    <button onClick={onDecrement}>-</button>
-  </div>
-);
+const Counter = props => {
+  console.log(props);
+  return (
+    <div className="App">
+      <h1>Simple counter</h1>
+    </div>
+  );
+};
 
 Counter.propTypes = {
-  state: PropTypes.number.isRequired,
+  // state: PropTypes.number.isRequired,
   onIncrement: PropTypes.func.isRequired,
   onDecrement: PropTypes.func.isRequired,
 };
@@ -29,9 +29,9 @@ const mapDispatchToProps = ({ dispatch }) => ({
   },
 });
 
-const mapStateToProps = ({ state }) => ({
-  state,
-});
+const mapStateToProps = ({ state }) => {
+  console.log(state);
+};
 
 const App = connect(mapDispatchToProps, mapStateToProps)(Counter);
 
