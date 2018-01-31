fea5f671dd571e75135e8ec944718e065d3352ff
Максим Юрьевич Алёшин <mgreen097@gmail.com>
2018-01-31 18:18:23 +0300

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './components/App';
-// import './index.scss';
+import './index.scss';
 
 import TodoApp from './reducers/';
 
