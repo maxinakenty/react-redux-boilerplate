28400186d04a5547c6264de165b1c6c08e8b38d0
Максим Юрьевич Алёшин <mgreen097@gmail.com>
2018-03-16 10:12:26 +0300

    add redux-dev-tools with middleware

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import App from './components/App';
-import './index.scss';
-
-import TodoApp from './reducers/';
+import React from "react";
+import { render } from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import App from "./components/App";
+import TodoApp from "./reducers/";
+import "./index.scss";
 
 /* eslint-disable no-underscore-dangle */
-const store = createStore(
-  TodoApp,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
 /* eslint-enable */
+
+// composeEnhancers(applyMiddleware(...your middleware))
+const enhancer = composeEnhancers(applyMiddleware());
+const store = createStore(TodoApp, enhancer);
 
 render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
