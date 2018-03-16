86066c3cbb28f99602be24143c111b325040d531
maxinakenty <mgreen097@gmail.com>
2018-03-16 22:11:19 +0300

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { render } from "react-dom";
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import { composeWithDevTools } from "redux-devtools-extension/logOnlyInProduction";
-import App from "./components/App";
-import TodoApp from "./reducers/";
-import "./index.scss";
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
+import App from './components/App';
+import TodoApp from './reducers/';
+import './index.scss';
 
 // React the documentation:
 // https://github.com/zalmoxisus/redux-devtools-extension#14-using-in-production
@@ -17,14 +17,14 @@ const store = createStore(
   TodoApp,
   /* preloadedState, */ composeEnhancers(
     // applyMiddleware(...your middleware)
-    applyMiddleware()
+    applyMiddleware(),
     // other store enhancers if any
-  )
+  ),
 );
 
 render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
