b3311b86826db7f137ec2d245a0e2096a98c13b7
maxinakenty <mgreen097@gmail.com>
2018-04-06 22:33:16 +0300

    fix Root.jsx

--- src/containers/Root.jsx
+++ src/containers/Root.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { hot } from 'react-hot-loader';
 import App from '../components/App';
 import rootReducer from '../reducers/';
 
-// React the documentation:
-// https://github.com/zalmoxisus/redux-devtools-extension#14-using-in-production
-const composeEnhancers = composeWithDevTools({
-  // options like actionSanitizer, stateSanitizer
-});
-
 const store = createStore(
   rootReducer,
-  /* preloadedState, */ composeEnhancers(
-    // applyMiddleware(...your middleware)
+  composeWithDevTools(
+    // applyMiddleware(...middleware)
     applyMiddleware(),
     // other store enhancers if any
   ),
