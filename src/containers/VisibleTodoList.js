4a64a1beddb26880504808fa6fad2a5ecb1784b9
maxinakenty <mgreen097@gmail.com>
2018-04-30 16:13:47 +0300

    add reselect

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,25 +1,10 @@
 import { connect } from 'react-redux';
-import { VisibilityFilters } from '../constants/ActionTypes';
 import { toggleTodo } from '../actionCreators';
 import TodoList from '../components/TodoList';
-
-const { SHOW_ALL, SHOW_ACTIVE, SHOW_COMPLETED } = VisibilityFilters;
-
-const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-    case SHOW_ALL:
-      return todos;
-    case SHOW_COMPLETED:
-      return todos.filter(t => t.completed);
-    case SHOW_ACTIVE:
-      return todos.filter(t => !t.completed);
-    default:
-      return todos;
-  }
-};
+import getVisibleTodos from '../selectors';
 
 const mapStateToProps = state => ({
-  todos: getVisibleTodos(state.todos, state.visibilityFilter),
+  todos: getVisibleTodos(state),
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/reducers/visibilityFilter.js
+++ src/reducers/visibilityFilter.js
@@ -1,17 +1,14 @@
-import {
-  SET_VISIBILITY_FILTER,
-  VisibilityFilters,
-} from '../constants/ActionTypes';
+import { handleActions } from 'redux-actions';
+import { setVisibilityFilter } from '../actionCreators';
+import { VisibilityFilters } from '../constants/ActionTypes';
 
 const { SHOW_ALL } = VisibilityFilters;
 
-const visibilityFilter = (state = SHOW_ALL, action) => {
-  switch (action.type) {
-    case SET_VISIBILITY_FILTER:
-      return action.filter;
-    default:
-      return state;
-  }
-};
+const visibilityFilter = handleActions(
+  {
+    [setVisibilityFilter]: (state, action) => action.payload.filter,
+  },
+  SHOW_ALL,
+);
 
 export default visibilityFilter;
